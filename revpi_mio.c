2d1e260b705017c766dfaf4ece819c1e258d1d23
Zhi Han <z.han@kunbus.com>
2020-09-15 09:25:31 +0200

    add the support for aio request compress

--- revpi_mio.c
+++ revpi_mio.c
@@ -17,11 +17,16 @@
 #include "revpi_core.h"
 #include "revpi_mio.h"
 
-/*configurations of MIO modules*/
+/* configurations of MIO modules */
 static struct mio_config mio_conf_list[REVPI_MIO_MAX];
 
-/*the counter of the MIO module*/
+/* the counter of the MIO module */
 static int mio_conf_cnt;
+
+/* store the sent analog request.
+   the field i8uChannels of struct SMioAnalogRequestData takes no function here,
+   but it could be used for the debuging purpose */
+static  SMioAnalogRequestData mio_dio_request_last[REVPI_MIO_MAX];
 
 
 int revpi_mio_cycle_dio(SDevice *dev, SMioDigitalRequestData *req_data,
@@ -70,7 +75,7 @@ int revpi_mio_cycle_dio(SDevice *dev, SMioDigitalRequestData *req_data,
 }
 
 int revpi_mio_cycle_aio(SDevice *dev, SMioAnalogRequestData *req_data,
-											SMioAnalogResponseData *resp_data)
+							size_t ch_cnt, SMioAnalogResponseData *resp_data)
 {
 	int ret;
 
@@ -78,24 +83,30 @@ int revpi_mio_cycle_aio(SDevice *dev, SMioAnalogRequestData *req_data,
 	SMioAnalogResponse resp;
 	unsigned char crc_cal;
 
+	size_t compressed = (MIO_AIO_PORT_CNT - ch_cnt) * sizeof(INT16U);
+
 	revpi_io_build_header(&req.uHeader,
 							dev->i8uAddress,
-							sizeof(SMioAnalogRequestData),
+							sizeof(SMioAnalogRequestData) - compressed,
 							IOP_TYP1_CMD_DATA2);
 
 	/*copy: from process image:output to request*/
 	rt_mutex_lock(&piDev_g.lockPI);
-	memcpy(&req.sData, req_data, sizeof(SMioAnalogRequestData));
+	memcpy(&req.sData, req_data, sizeof(SMioAnalogRequestData) - compressed);
 	rt_mutex_unlock(&piDev_g.lockPI);
 
-	req.i8uCrc = revpi_crc8(&req, sizeof(req) - 1);
+	req.i8uCrc = revpi_crc8(&req, sizeof(req) - 1 - compressed);
+	memcpy(&req.i8uCrc - compressed, &req.i8uCrc, 1); /*crc is adjoining data */
 
-	ret = revpi_io_talk(&req, sizeof(req), &resp, sizeof(resp));
+	//pr_info_ratelimited("req:%*ph\n", sizeof(req) - compressed, &req);
+
+	ret = revpi_io_talk(&req, sizeof(req) - compressed, &resp, sizeof(resp));
 	if(ret) {
 		pr_err_ratelimited("talk with mio for aio data error(addr:%d, ret:%d)\n",
 														dev->i8uAddress, ret);
 		return -EIO;
 	}
+	//pr_info_ratelimited("resp:%*ph\n", sizeof(resp), &resp);
 
 	crc_cal = revpi_crc8(&resp, sizeof(resp) - 1);
 	if (resp.i8uCrc != crc_cal) {
@@ -115,14 +126,72 @@ int revpi_mio_cycle_aio(SDevice *dev, SMioAnalogRequestData *req_data,
 	return 0;
 }
 
+/*
+	compare to get the changed values of channels
+	input parameters:
+		a, b: data of two messages
+		count: count of channels
+		step: number of bytes for a channel
+	return:	bit map of changed channels
+*/
+unsigned long revpi_chnl_cmp(void *a, void *b, int count, int step)
+{
+	int i;
+	unsigned long ret;
+	unsigned char *pa, *pb;
+
+	pa = (unsigned char *)a;
+	pb = (unsigned char *)b;
+
+	for (i = 0; i < count; i++) {
+		if (memcmp(pa + i*step, pb + i*step, step)) {
+			set_bit(i, &ret);
+		}
+	}
+	return ret;
+}
+
+#define VALUE_MASK(v, h, l) ((v) & (GENMASK((h), (l))))
+#define VALUE_MASK_L(v, h) VALUE_MASK((v), (h), 0)
+#define VALUE_MASK_H(v, l) VALUE_MASK((v), (BITS_PER_LONG - 1), (l))
+
+/*
+	compress the channel, only data of changed channel will be taken
+	input parameters:
+		dst, dst: compress from src to dst
+		bitmap: compress according to
+		step: number of bytes for a channel
+	return: the count of channels has been taken.
+*/
+unsigned int revpi_chnl_compress(void *dst, void *src, unsigned long bitmap, int step)
+{
+	unsigned char *d, *s;
+	unsigned int i = 0;
+
+	s = (unsigned char *)src;
+	d = (unsigned char *)dst;
+
+	for(i = 0; i < 32 && VALUE_MASK_H(bitmap, i); i++) {
+		if (test_bit(i, &bitmap)) {
+			memcpy(d, s + i * step, step);
+			d += step;
+		}
+	}
+	return (d - (unsigned char *)dst) / step;
+}
+
 int revpi_mio_cycle(unsigned char devno)
 {
 	int ret;
 	SDevice *dev;
 	mio_process_image_out *proc_img_out;
 	mio_process_image_in *proc_img_in;
+	SMioAnalogRequestData *last;
+	SMioAnalogRequestData io_req_ex;
+	unsigned int ch_cnt = 0;
 
 	dev = RevPiDevice_getDev(devno);
+	last = &mio_dio_request_last[dev->i8uPriv];
 
 	proc_img_out = (mio_process_image_out *)(piDev_g.ai8uPI + dev->i16uOutputOffset);
 	proc_img_in = (mio_process_image_in *)(piDev_g.ai8uPI + dev->i16uInputOffset);
@@ -134,7 +203,29 @@ int revpi_mio_cycle(unsigned char devno)
 		return ret;
 	}
 
-	ret = revpi_mio_cycle_aio(dev, &proc_img_out->aio, &proc_img_in->aio);
+	/* for the AIO cycle */
+	io_req_ex.i8uChannels = revpi_chnl_cmp(&last->i16uOutputVoltage,
+											&proc_img_out->aio.i16uOutputVoltage,
+											MIO_AIO_PORT_CNT,
+											2);
+	/* force to update from process image */
+	io_req_ex.i8uChannels |= proc_img_out->aio.i8uChannels;
+
+	if(io_req_ex.i8uChannels) {
+		memcpy(&last->i16uOutputVoltage,
+				&proc_img_out->aio.i16uOutputVoltage,
+				sizeof(unsigned short) * MIO_AIO_PORT_CNT);
+
+		ch_cnt = revpi_chnl_compress(&io_req_ex.i16uOutputVoltage,
+									&proc_img_out->aio.i16uOutputVoltage,
+									io_req_ex.i8uChannels,
+									2);
+
+		pr_info("copy bitmap, bitmap:0x%x, copied len:%d, data:%*ph\n",
+													io_req_ex.i8uChannels, ch_cnt,
+													17,&io_req_ex);
+	}
+	ret = revpi_mio_cycle_aio(dev, &io_req_ex, ch_cnt, &proc_img_in->aio);
 	if (ret) {
 		return ret;
 	}
@@ -260,6 +351,7 @@ int revpi_mio_init(unsigned char devno)
 		if (mio_conf_list[i].dio.uHeader.sHeaderTyp1.bitAddress == addr) {
 
 			conf = &mio_conf_list[i];
+			RevPiDevice_getDev(devno)->i8uPriv = (unsigned char)i;
 
 			/*the address of dio and aio (in and out) should be the same*/
 			if ((conf->aio_i.uHeader.sHeaderTyp1.bitAddress != addr )
