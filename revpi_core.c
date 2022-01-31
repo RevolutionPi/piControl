0cdb0dd5f530079c0e57def1502a1a2fb9625db5
Lino Sanfilippo <l.sanfilippo@kunbus.com>
2022-01-31 13:50:57 +0100

    revpi: core: register platform driver for revpi core device
    Make use of the new "pibridge" device tree entry and register a platform
    driver that is compatible to this node. Move the device initialization/
    deinitialization to the drivers probe/remove functions.
    With this we dont have to choose the GPIO controller by name but get
    the sniff pins out of the pibridge entry.
    
    Signed-off-by: Lino Sanfilippo <l.sanfilippo@kunbus.com>

--- revpi_core.c
+++ revpi_core.c
@@ -47,17 +47,6 @@ static const struct kthread_prio revpi_core_kthread_prios[] = {
 	{ }
 };
 
-static struct gpiod_lookup_table revpi_core_gpios = {
-	.dev_id = "piControl0",
-	.table  = { GPIO_LOOKUP_IDX("pinctrl-bcm2835", 42, "Sniff1A", 0, GPIO_ACTIVE_HIGH),	// 1 links
-		    GPIO_LOOKUP_IDX("pinctrl-bcm2835", 43, "Sniff1B", 0, GPIO_ACTIVE_HIGH),	// 1 rechts
-		    GPIO_LOOKUP_IDX("pinctrl-bcm2835", 28, "Sniff2A", 0, GPIO_ACTIVE_HIGH),	// 2 links
-		    GPIO_LOOKUP_IDX("pinctrl-bcm2835", 29, "Sniff2B", 0, GPIO_ACTIVE_HIGH),	// 2 rechts
-	},
-};
-
-// im Schaltplan heißen die Pins 1B und 2B, da sie links sind, müssten sie aber 1A und 1B heißen.
-// hier in der Software werden sie mit A benannt
 static struct gpiod_lookup_table revpi_connect_gpios = {
 	.dev_id = "piControl0",
 	.table  = { GPIO_LOOKUP_IDX("pinctrl-bcm2835", 43, "Sniff1A",	0, GPIO_ACTIVE_HIGH),	// 1 links
@@ -235,19 +224,149 @@ static int piIoThread(void *data)
 	return 0;
 }
 
-int revpi_core_init(void)
+static void deinit_sniff_gpios(void)
 {
-	int ret = 0;
+	/* reset GPIO direction */
+	piIoComm_writeSniff1A(enGpioValue_Low, enGpioMode_Input);
+	piIoComm_writeSniff2A(enGpioValue_Low, enGpioMode_Input);
+
+	if (piDev_g.machine_type == REVPI_CORE) {
+		piIoComm_writeSniff1B(enGpioValue_Low, enGpioMode_Input);
+		piIoComm_writeSniff2B(enGpioValue_Low, enGpioMode_Input);
+	}
+}
+
+static void deinit_connect_gpios(void)
+{
+	gpiod_put(piCore_g.gpio_wdtrigger);
+	gpiod_put(piCore_g.gpio_x2do);
+	gpiod_put(piCore_g.gpio_x2di);
+	gpiod_remove_lookup_table(&revpi_connect_gpios);
+}
+
+static void deinit_gpios(void)
+{
+	deinit_sniff_gpios();
+	if (piDev_g.machine_type == REVPI_CONNECT)
+		deinit_connect_gpios();
+}
+
+static int init_sniff_gpios(struct platform_device *pdev)
+{
+	struct gpio_descs *descs;
+
+	descs = devm_gpiod_get_array(&pdev->dev, "left-sniff", GPIOD_IN);
+	if (IS_ERR(descs)) {
+		dev_err(&pdev->dev, "Failed to get left sniff gpios\n");
+		return PTR_ERR(descs);
+	}
+
+	if (descs->ndescs != 2) {
+		dev_err(&pdev->dev, "Invalid number of left sniff gpios (%u)\n",
+			descs->ndescs);
+		return -ENXIO;
+	}
+	/*
+	 * Note: in the schematic for the connect these pins are named 1B
+	 * and 2B although since they are for the left side, they should
+	 * rather be named 1A and 1B. However here in software we name them
+	 * 1a and 2a for both connect and core.
+	 */
+	piCore_g.gpio_sniff1a = descs->desc[0];
+	piCore_g.gpio_sniff2a = descs->desc[1];
+
+	if (piDev_g.machine_type == REVPI_CORE) {
+		descs = devm_gpiod_get_array(&pdev->dev, "right-sniff", GPIOD_IN);
+		if (IS_ERR(descs)) {
+			dev_err(&pdev->dev, "Failed to get right sniff gpios\n");
+			return PTR_ERR(descs);
+		}
+
+		if (descs->ndescs != 2) {
+			dev_err(&pdev->dev, "Invalid number of right sniff gpios (%u)\n",
+				descs->ndescs);
+			return -ENXIO;
+		}
+		piCore_g.gpio_sniff1b = descs->desc[0];
+		piCore_g.gpio_sniff2b = descs->desc[1];
+	}
+
+	return 0;
+}
+
+static int init_connect_gpios(void)
+{
+	int ret;
+	// the connect has only a only one modular gateway port on the left
+	gpiod_add_lookup_table(&revpi_connect_gpios);
+
+	piCore_g.gpio_x2di = gpiod_get(piDev_g.dev, "X2_DI", GPIOD_IN);
+	if (IS_ERR(piCore_g.gpio_x2di)) {
+		pr_err("cannot acquire gpio x2 di\n");
+		ret = PTR_ERR(piCore_g.gpio_x2di);
+		goto err_remove_table;
+	}
+	piCore_g.gpio_x2do = gpiod_get(piDev_g.dev, "X2_DO", GPIOD_OUT_LOW);
+	if (IS_ERR(piCore_g.gpio_x2do)) {
+		pr_err("cannot acquire gpio x2 do\n");
+		ret = PTR_ERR(piCore_g.gpio_x2do);
+		goto err_free_x2di;
+	}
+	piCore_g.gpio_wdtrigger = gpiod_get(piDev_g.dev, "WDTrigger", GPIOD_OUT_LOW);
+	if (IS_ERR(piCore_g.gpio_wdtrigger)) {
+		pr_err("cannot acquire gpio watchdog trigger\n");
+		ret = PTR_ERR(piCore_g.gpio_wdtrigger);
+		goto err_free_x2do;
+	}
+
+	return 0;
+
+err_free_x2do:
+	gpiod_put(piCore_g.gpio_x2do);
+err_free_x2di:
+	gpiod_put(piCore_g.gpio_x2di);
+err_remove_table:
+	gpiod_remove_lookup_table(&revpi_connect_gpios);
+
+	return ret;
+}
+
+static int init_gpios(struct platform_device *pdev)
+{
+	int ret;
+
+	ret = init_sniff_gpios(pdev);
+	if (ret) {
+		dev_err(piDev_g.dev, "Failed to init sniff gpios: %i\n", ret);
+		return ret;
+	}
+
+	if (piDev_g.machine_type == REVPI_CONNECT) {
+		ret = init_connect_gpios();
+		if (ret) {
+			dev_err(piDev_g.dev, "Failed to init connect gpios: %i\n",
+				ret);
+			goto err_deinit_sniff_gpios;
+		}
+	}
+	return 0;
+
+err_deinit_sniff_gpios:
+	deinit_sniff_gpios();
+	return ret;
+}
+
+static int pibridge_probe(struct platform_device *pdev)
+{
+	int ret;
 
 	piCore_g.i8uLeftMGateIdx = REV_PI_DEV_UNDEF;
 	piCore_g.i8uRightMGateIdx = REV_PI_DEV_UNDEF;
 
-	if (piDev_g.machine_type == REVPI_CORE) {
-		// the Core has two modular gateway ports
-		gpiod_add_lookup_table(&revpi_core_gpios);
-	} else if (piDev_g.machine_type == REVPI_CONNECT) {
-		// the connect has only a only one modular gateway port on the left
-		gpiod_add_lookup_table(&revpi_connect_gpios);
+	ret = init_gpios(pdev);
+	if (ret) {
+		dev_err(piDev_g.dev, "Failed to init gpios: %i\n", ret);
+		return ret;
 	}
 
 	rt_mutex_init(&piCore_g.lockUserTel);
@@ -264,62 +383,14 @@ int revpi_core_init(void)
 	piCore_g.fw = revpi_get_firmware();
 	if (!piCore_g.fw) {
 		dev_err(piDev_g.dev, "Failed to get revpi firmware\n");
-		return -ENXIO;
-	}
-
-	piCore_g.gpio_sniff1a = gpiod_get(piDev_g.dev, "Sniff1A", GPIOD_IN);
-	if (IS_ERR(piCore_g.gpio_sniff1a)) {
-		pr_err("cannot acquire gpio sniff 1a\n");
-		ret = PTR_ERR(piCore_g.gpio_sniff1a);
-		goto err_remove_table;
-	}
-	piCore_g.gpio_sniff2a = gpiod_get(piDev_g.dev, "Sniff2A", GPIOD_IN);
-	if (IS_ERR(piCore_g.gpio_sniff2a)) {
-		pr_err("cannot acquire gpio sniff 2a\n");
-		ret = PTR_ERR(piCore_g.gpio_sniff2a);
-		goto err_gpiod_put;
-	}
-	if (piDev_g.machine_type == REVPI_CORE) {
-		piCore_g.gpio_sniff1b = gpiod_get(piDev_g.dev, "Sniff1B", GPIOD_IN);
-		if (IS_ERR(piCore_g.gpio_sniff1b)) {
-			pr_err("cannot acquire gpio sniff 1b\n");
-			ret = PTR_ERR(piCore_g.gpio_sniff1b);
-			goto err_gpiod_put;
-		}
-		piCore_g.gpio_sniff2b = gpiod_get(piDev_g.dev, "Sniff2B", GPIOD_IN);
-		if (IS_ERR(piCore_g.gpio_sniff2b)) {
-			pr_err("cannot acquire gpio sniff 2b\n");
-			ret = PTR_ERR(piCore_g.gpio_sniff2b);
-			goto err_gpiod_put;
-		}
-	}
-	if (piDev_g.machine_type == REVPI_CONNECT) {
-		piCore_g.gpio_x2di = gpiod_get(piDev_g.dev, "X2_DI", GPIOD_IN);
-		if (IS_ERR(piCore_g.gpio_x2di)) {
-			pr_err("cannot acquire gpio x2 di\n");
-			ret = PTR_ERR(piCore_g.gpio_x2di);
-			goto err_gpiod_put;
-		}
-		piCore_g.gpio_x2do = gpiod_get(piDev_g.dev, "X2_DO", GPIOD_OUT_LOW);
-		if (IS_ERR(piCore_g.gpio_x2do)) {
-			pr_err("cannot acquire gpio x2 do\n");
-			ret = PTR_ERR(piCore_g.gpio_x2do);
-			goto err_gpiod_put;
-		}
-		piCore_g.gpio_wdtrigger = gpiod_get(piDev_g.dev, "WDTrigger", GPIOD_OUT_LOW);
-		if (IS_ERR(piCore_g.gpio_wdtrigger)) {
-			pr_err("cannot acquire gpio watchdog trigger\n");
-			ret = PTR_ERR(piCore_g.gpio_wdtrigger);
-			goto err_gpiod_put;
-		}
+		goto err_deinit_gpios;
 	}
 
 	if (piIoComm_init()) {
 		pr_err("open serial port failed\n");
 		ret = -EFAULT;
-		goto err_gpiod_put;
+		goto err_release_fw;
 	}
-
 	/* run threads */
 	ret = set_kthread_prios(revpi_core_kthread_prios);
 	if (ret)
@@ -341,41 +412,21 @@ int revpi_core_init(void)
 	}
 	sched_set_fifo(piCore_g.pIoThread);
 
-	return ret;
+	return 0;
 
 err_stop_uart_thread:
 	kthread_stop(piCore_g.pUartThread);
 err_close_serial:
 	piIoComm_finish();
-err_gpiod_put:
-	if (!IS_ERR_OR_NULL(piCore_g.gpio_sniff1a))
-		gpiod_put(piCore_g.gpio_sniff1a);
-	if (!IS_ERR_OR_NULL(piCore_g.gpio_sniff2a))
-		gpiod_put(piCore_g.gpio_sniff2a);
-	if (piDev_g.machine_type == REVPI_CORE) {
-		if (!IS_ERR_OR_NULL(piCore_g.gpio_sniff1b))
-			gpiod_put(piCore_g.gpio_sniff1b);
-		if (!IS_ERR_OR_NULL(piCore_g.gpio_sniff2b))
-			gpiod_put(piCore_g.gpio_sniff2b);
-	} else if (piDev_g.machine_type == REVPI_CONNECT) {
-		if (!IS_ERR_OR_NULL(piCore_g.gpio_x2di))
-			gpiod_put(piCore_g.gpio_x2di);
-		if (!IS_ERR_OR_NULL(piCore_g.gpio_x2do))
-			gpiod_put(piCore_g.gpio_x2do);
-		if (!IS_ERR_OR_NULL(piCore_g.gpio_wdtrigger))
-			gpiod_put(piCore_g.gpio_wdtrigger);
-	}
-err_remove_table:
-	if (piDev_g.machine_type == REVPI_CORE)
-		gpiod_remove_lookup_table(&revpi_core_gpios);
-	else if (piDev_g.machine_type == REVPI_CONNECT)
-		gpiod_remove_lookup_table(&revpi_connect_gpios);
+err_release_fw:
 	revpi_release_firmware(piCore_g.fw);
+err_deinit_gpios:
+	deinit_gpios();
 
 	return ret;
 }
 
-void revpi_core_fini(void)
+static int pibridge_remove(struct platform_device *pdev)
 {
 	/* tell UART thread to cancel the main loop */
 	send_sig(SIGTERM, piCore_g.pUartThread, 1);
@@ -383,27 +434,38 @@ void revpi_core_fini(void)
 	kthread_stop(piCore_g.pIoThread);
 	kthread_stop(piCore_g.pUartThread);
 	piIoComm_finish();
-
-	/* reset GPIO direction */
-	piIoComm_writeSniff1A(enGpioValue_Low, enGpioMode_Input);
-	gpiod_put(piCore_g.gpio_sniff1a);
-	piIoComm_writeSniff2A(enGpioValue_Low, enGpioMode_Input);
-	gpiod_put(piCore_g.gpio_sniff2a);
-	if (piDev_g.machine_type == REVPI_CORE) {
-		piIoComm_writeSniff1B(enGpioValue_Low, enGpioMode_Input);
-		gpiod_put(piCore_g.gpio_sniff1b);
-		piIoComm_writeSniff2B(enGpioValue_Low, enGpioMode_Input);
-		gpiod_put(piCore_g.gpio_sniff2b);
-	} else if (piDev_g.machine_type == REVPI_CONNECT) {
-		gpiod_put(piCore_g.gpio_x2di);
-		gpiod_put(piCore_g.gpio_x2do);
-		gpiod_put(piCore_g.gpio_wdtrigger);
-	}
-
-	if (piDev_g.machine_type == REVPI_CORE)
-		gpiod_remove_lookup_table(&revpi_core_gpios);
-	else if (piDev_g.machine_type == REVPI_CONNECT)
-		gpiod_remove_lookup_table(&revpi_connect_gpios);
-
 	revpi_release_firmware(piCore_g.fw);
+	deinit_gpios();
+
+	return 0;
+}
+
+static const struct of_device_id of_pibridge_match[] = {
+	{ .compatible = "kunbus,pibridge", },
+	{},
+};
+
+static struct platform_driver pibridge_driver = {
+	.probe		= pibridge_probe,
+	.remove 	= pibridge_remove,
+	.driver		= {
+		.name	= "pibridge",
+		.of_match_table = of_pibridge_match,
+	},
+};
+
+int revpi_core_init(void)
+{
+	int ret;
+
+	ret = platform_driver_register(&pibridge_driver);
+	if (ret)
+		pr_err("failed to register pibridge driver: %i\n", ret);
+
+	return ret;
+}
+
+void revpi_core_fini(void)
+{
+	platform_driver_unregister(&pibridge_driver);
 }
