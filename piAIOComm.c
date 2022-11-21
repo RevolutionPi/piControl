6700b4a4b24727fa9f0e2366da22c71f49fe4c7e
Lino Sanfilippo <l.sanfilippo@kunbus.com>
2022-11-21 19:10:18 +0100

    piControl: use pibridge_req_io() to send AIO config messages
    In piAIOComm_Init() functions piIoComm_send() and piIoComm_recv() are used
    to exchange configuration messages with the AIO module. The complete
    send-receive logic is also implemented in pibridge_req_io(). So use this
    function instead to avoid code duplicity.
    
    Since the message header and CRC are already handled by pibridge_req_io()
    remove both from the respective message structs and adjust the code in
    piAIOComm_Init() accordingly.
    
    Signed-off-by: Lino Sanfilippo <l.sanfilippo@kunbus.com>

--- piAIOComm.c
+++ piAIOComm.c
@@ -47,52 +47,40 @@
 #include <piIOComm.h>
 #include <piAIOComm.h>
 
+#define AIO_MAX_DEVS			10
 #define AIO_OUTPUT_DATA_LEN		sizeof(SAioRequest)
 #define AIO_INPUT_DATA_LEN		sizeof(SAioResponse)
+#define AIO_CONFIG_DATA2_LEN		sizeof(SAioInConfig)
+#define AIO_CONFIG_DATA3_LEN		sizeof(SAioInConfig)
+#define AIO_CONFIG_DATA1_LEN		sizeof(SAioConfig)
 
-static INT8U i8uConfigured_s = 0;
-static SAioConfig aioConfig_s[10];
-static SAioInConfig aioIn1Config_s[10];
-static SAioInConfig aioIn2Config_s[10];
+static unsigned int num_aios = 0;
+static SAioConfig aioConfig_s[AIO_MAX_DEVS];
+static SAioInConfig aioIn1Config_s[AIO_MAX_DEVS];
+static SAioInConfig aioIn2Config_s[AIO_MAX_DEVS];
+
+static u8 aio_dev[AIO_MAX_DEVS];
 
 void piAIOComm_InitStart(void)
 {
 	pr_info_aio("piAIOComm_InitStart\n");
-	i8uConfigured_s = 0;
+	num_aios = 0;
 }
 
-INT32U piAIOComm_Config(uint8_t i8uAddress, uint16_t i16uNumEntries, SEntryInfo * pEnt)
+u32 piAIOComm_Config(u8 addr, u16 num_entries, SEntryInfo * pEnt)
 {
 	uint16_t i;
 
-	if (i8uConfigured_s >= sizeof(aioConfig_s) / sizeof(SAioConfig)) {
+	if (num_aios >= AIO_MAX_DEVS) {
 		pr_err("max. number of AIOs reached\n");
 		return -1;
 	}
 
-	pr_info_aio("piAIOComm_Config addr %d entries %d  num %d\n", i8uAddress, i16uNumEntries, i8uConfigured_s);
-	memset(&aioConfig_s[i8uConfigured_s], 0, sizeof(SAioConfig));
+	pr_info_aio("piAIOComm_Config addr %d entries %d  num %d\n", addr, num_entries, num_aios);
 
-	aioConfig_s[i8uConfigured_s].uHeader.sHeaderTyp1.bitAddress = i8uAddress;
-	aioConfig_s[i8uConfigured_s].uHeader.sHeaderTyp1.bitIoHeaderType = 0;
-	aioConfig_s[i8uConfigured_s].uHeader.sHeaderTyp1.bitReqResp = 0;
-	aioConfig_s[i8uConfigured_s].uHeader.sHeaderTyp1.bitLength = sizeof(SAioConfig) - IOPROTOCOL_HEADER_LENGTH - 1;
-	aioConfig_s[i8uConfigured_s].uHeader.sHeaderTyp1.bitCommand = IOP_TYP1_CMD_CFG;
+	aio_dev[num_aios] = addr;
 
-	aioIn1Config_s[i8uConfigured_s].uHeader.sHeaderTyp1.bitAddress = i8uAddress;
-	aioIn1Config_s[i8uConfigured_s].uHeader.sHeaderTyp1.bitIoHeaderType = 0;
-	aioIn1Config_s[i8uConfigured_s].uHeader.sHeaderTyp1.bitReqResp = 0;
-	aioIn1Config_s[i8uConfigured_s].uHeader.sHeaderTyp1.bitLength = sizeof(SAioInConfig) - IOPROTOCOL_HEADER_LENGTH - 1;
-	aioIn1Config_s[i8uConfigured_s].uHeader.sHeaderTyp1.bitCommand = IOP_TYP1_CMD_DATA2;
-
-	aioIn2Config_s[i8uConfigured_s].uHeader.sHeaderTyp1.bitAddress = i8uAddress;
-	aioIn2Config_s[i8uConfigured_s].uHeader.sHeaderTyp1.bitIoHeaderType = 0;
-	aioIn2Config_s[i8uConfigured_s].uHeader.sHeaderTyp1.bitReqResp = 0;
-	aioIn2Config_s[i8uConfigured_s].uHeader.sHeaderTyp1.bitLength = sizeof(SAioInConfig) - IOPROTOCOL_HEADER_LENGTH - 1;
-	aioIn2Config_s[i8uConfigured_s].uHeader.sHeaderTyp1.bitCommand = IOP_TYP1_CMD_DATA3;
-
-
-	for (i = 0; i < i16uNumEntries; i++) {
+	for (i = 0; i < num_entries; i++) {
 		pr_info_aio("addr %2d  type %d  len %3d  offset %3d  value %d 0x%x\n",
 			    pEnt[i].i8uAddress, pEnt[i].i8uType, pEnt[i].i16uBitLength, pEnt[i].i16uOffset,
 			    pEnt[i].i32uDefault, pEnt[i].i32uDefault);
@@ -117,231 +105,194 @@ INT32U piAIOComm_Config(uint8_t i8uAddress, uint16_t i16uNumEntries, SEntryInfo 
 			// nothing to do
 			break;
 		case AIO_OFFSET_Input1Range:
-			aioIn1Config_s[i8uConfigured_s].sAioInputConfig[0].eInputRange = pEnt[i].i32uDefault;
+			aioIn1Config_s[num_aios].sAioInputConfig[0].eInputRange = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Input1Factor:
-			aioIn1Config_s[i8uConfigured_s].sAioInputConfig[0].i16sA1 = pEnt[i].i32uDefault;
+			aioIn1Config_s[num_aios].sAioInputConfig[0].i16sA1 = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Input1Divisor:
-			aioIn1Config_s[i8uConfigured_s].sAioInputConfig[0].i16uA2 = pEnt[i].i32uDefault;
+			aioIn1Config_s[num_aios].sAioInputConfig[0].i16uA2 = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Input1Offset:
-			aioIn1Config_s[i8uConfigured_s].sAioInputConfig[0].i16sB = pEnt[i].i32uDefault;
+			aioIn1Config_s[num_aios].sAioInputConfig[0].i16sB = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Input2Range:
-			aioIn1Config_s[i8uConfigured_s].sAioInputConfig[1].eInputRange = pEnt[i].i32uDefault;
+			aioIn1Config_s[num_aios].sAioInputConfig[1].eInputRange = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Input2Factor:
-			aioIn1Config_s[i8uConfigured_s].sAioInputConfig[1].i16sA1 = pEnt[i].i32uDefault;
+			aioIn1Config_s[num_aios].sAioInputConfig[1].i16sA1 = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Input2Divisor:
-			aioIn1Config_s[i8uConfigured_s].sAioInputConfig[1].i16uA2 = pEnt[i].i32uDefault;
+			aioIn1Config_s[num_aios].sAioInputConfig[1].i16uA2 = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Input2Offset:
-			aioIn1Config_s[i8uConfigured_s].sAioInputConfig[1].i16sB = pEnt[i].i32uDefault;
+			aioIn1Config_s[num_aios].sAioInputConfig[1].i16sB = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Input3Range:
-			aioIn2Config_s[i8uConfigured_s].sAioInputConfig[0].eInputRange = pEnt[i].i32uDefault;
+			aioIn2Config_s[num_aios].sAioInputConfig[0].eInputRange = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Input3Factor:
-			aioIn2Config_s[i8uConfigured_s].sAioInputConfig[0].i16sA1 = pEnt[i].i32uDefault;
+			aioIn2Config_s[num_aios].sAioInputConfig[0].i16sA1 = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Input3Divisor:
-			aioIn2Config_s[i8uConfigured_s].sAioInputConfig[0].i16uA2 = pEnt[i].i32uDefault;
+			aioIn2Config_s[num_aios].sAioInputConfig[0].i16uA2 = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Input3Offset:
-			aioIn2Config_s[i8uConfigured_s].sAioInputConfig[0].i16sB = pEnt[i].i32uDefault;
+			aioIn2Config_s[num_aios].sAioInputConfig[0].i16sB = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Input4Range:
-			aioIn2Config_s[i8uConfigured_s].sAioInputConfig[1].eInputRange = pEnt[i].i32uDefault;
+			aioIn2Config_s[num_aios].sAioInputConfig[1].eInputRange = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Input4Factor:
-			aioIn2Config_s[i8uConfigured_s].sAioInputConfig[1].i16sA1 = pEnt[i].i32uDefault;
+			aioIn2Config_s[num_aios].sAioInputConfig[1].i16sA1 = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Input4Divisor:
-			aioIn2Config_s[i8uConfigured_s].sAioInputConfig[1].i16uA2 = pEnt[i].i32uDefault;
+			aioIn2Config_s[num_aios].sAioInputConfig[1].i16uA2 = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Input4Offset:
-			aioIn2Config_s[i8uConfigured_s].sAioInputConfig[1].i16sB = pEnt[i].i32uDefault;
+			aioIn2Config_s[num_aios].sAioInputConfig[1].i16sB = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_InputSampleRate:
-			aioConfig_s[i8uConfigured_s].i8uInputSampleRate = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].i8uInputSampleRate = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_RTD1Type:
-			aioConfig_s[i8uConfigured_s].sAioRtdConfig[0].i8uSensorType = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioRtdConfig[0].i8uSensorType = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_RTD1Method:
 			if (pEnt[i].i32uDefault == 1)
-				aioConfig_s[i8uConfigured_s].sAioRtdConfig[0].i8uMeasureMethod = 1;	// 4 wire
+				aioConfig_s[num_aios].sAioRtdConfig[0].i8uMeasureMethod = 1;	// 4 wire
 			else
-				aioConfig_s[i8uConfigured_s].sAioRtdConfig[0].i8uMeasureMethod = 0;	// 2 or 3 wire
+				aioConfig_s[num_aios].sAioRtdConfig[0].i8uMeasureMethod = 0;	// 2 or 3 wire
 			break;
 		case AIO_OFFSET_RTD1Factor:
-			aioConfig_s[i8uConfigured_s].sAioRtdConfig[0].i16sA1 = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioRtdConfig[0].i16sA1 = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_RTD1Divisor:
-			aioConfig_s[i8uConfigured_s].sAioRtdConfig[0].i16uA2 = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioRtdConfig[0].i16uA2 = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_RTD1Offset:
-			aioConfig_s[i8uConfigured_s].sAioRtdConfig[0].i16sB = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioRtdConfig[0].i16sB = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_RTD2Type:
-			aioConfig_s[i8uConfigured_s].sAioRtdConfig[1].i8uSensorType = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioRtdConfig[1].i8uSensorType = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_RTD2Method:
 			if (pEnt[i].i32uDefault == 1)
-				aioConfig_s[i8uConfigured_s].sAioRtdConfig[1].i8uMeasureMethod = 1;	// 4 wire
+				aioConfig_s[num_aios].sAioRtdConfig[1].i8uMeasureMethod = 1;	// 4 wire
 			else
-				aioConfig_s[i8uConfigured_s].sAioRtdConfig[1].i8uMeasureMethod = 0;	// 2 or 3 wire
+				aioConfig_s[num_aios].sAioRtdConfig[1].i8uMeasureMethod = 0;	// 2 or 3 wire
 			break;
 		case AIO_OFFSET_RTD2Factor:
-			aioConfig_s[i8uConfigured_s].sAioRtdConfig[1].i16sA1 = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioRtdConfig[1].i16sA1 = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_RTD2Divisor:
-			aioConfig_s[i8uConfigured_s].sAioRtdConfig[1].i16uA2 = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioRtdConfig[1].i16uA2 = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_RTD2Offset:
-			aioConfig_s[i8uConfigured_s].sAioRtdConfig[1].i16sB = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioRtdConfig[1].i16sB = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Output1Range:
-			aioConfig_s[i8uConfigured_s].sAioOutputConfig[0].eOutputRange = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioOutputConfig[0].eOutputRange = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Output1EnableSlew:
-			aioConfig_s[i8uConfigured_s].sAioOutputConfig[0].bSlewRateEnabled = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioOutputConfig[0].bSlewRateEnabled = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Output1SlewStepSize:
-			aioConfig_s[i8uConfigured_s].sAioOutputConfig[0].eSlewRateStepSize = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioOutputConfig[0].eSlewRateStepSize = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Output1SlewUpdateFreq:
-			aioConfig_s[i8uConfigured_s].sAioOutputConfig[0].eSlewRateFrequency = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioOutputConfig[0].eSlewRateFrequency = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Output1Factor:
-			aioConfig_s[i8uConfigured_s].sAioOutputConfig[0].i16sA1 = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioOutputConfig[0].i16sA1 = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Output1Divisor:
-			aioConfig_s[i8uConfigured_s].sAioOutputConfig[0].i16uA2 = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioOutputConfig[0].i16uA2 = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Output1Offset:
-			aioConfig_s[i8uConfigured_s].sAioOutputConfig[0].i16sB = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioOutputConfig[0].i16sB = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Output2Range:
-			aioConfig_s[i8uConfigured_s].sAioOutputConfig[1].eOutputRange = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioOutputConfig[1].eOutputRange = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Output2EnableSlew:
-			aioConfig_s[i8uConfigured_s].sAioOutputConfig[1].bSlewRateEnabled = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioOutputConfig[1].bSlewRateEnabled = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Output2SlewStepSize:
-			aioConfig_s[i8uConfigured_s].sAioOutputConfig[1].eSlewRateStepSize = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioOutputConfig[1].eSlewRateStepSize = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Output2SlewUpdateFreq:
-			aioConfig_s[i8uConfigured_s].sAioOutputConfig[1].eSlewRateFrequency = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioOutputConfig[1].eSlewRateFrequency = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Output2Factor:
-			aioConfig_s[i8uConfigured_s].sAioOutputConfig[1].i16sA1 = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioOutputConfig[1].i16sA1 = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Output2Divisor:
-			aioConfig_s[i8uConfigured_s].sAioOutputConfig[1].i16uA2 = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioOutputConfig[1].i16uA2 = pEnt[i].i32uDefault;
 			break;
 		case AIO_OFFSET_Output2Offset:
-			aioConfig_s[i8uConfigured_s].sAioOutputConfig[1].i16sB = pEnt[i].i32uDefault;
+			aioConfig_s[num_aios].sAioOutputConfig[1].i16sB = pEnt[i].i32uDefault;
 			break;
 		default:
 			pr_err("piAIOComm_Config: Unknown parameter %d in rsc-file\n", pEnt[i].i16uOffset);
 		}
 	}
-	aioConfig_s[i8uConfigured_s].i8uCrc =
-	    piIoComm_Crc8((INT8U *) & aioConfig_s[i8uConfigured_s], sizeof(SAioConfig) - 1);
 
-	aioIn1Config_s[i8uConfigured_s].i8uCrc =
-	    piIoComm_Crc8((INT8U *) & aioIn1Config_s[i8uConfigured_s], sizeof(SAioInConfig) - 1);
+	num_aios++;
 
-	aioIn2Config_s[i8uConfigured_s].i8uCrc =
-	    piIoComm_Crc8((INT8U *) & aioIn2Config_s[i8uConfigured_s], sizeof(SAioInConfig) - 1);
-
-	i8uConfigured_s++;
-	pr_info_aio("piAIOComm_Config done %d addr %d\n", i8uConfigured_s, i8uAddress);
+	pr_info_aio("piAIOComm_Config done %d addr %d\n", num_aios, addr);
 
 	return 0;
 }
 
-INT32U piAIOComm_Init(INT8U i8uDevice_p)
+u32 piAIOComm_Init(u8 devnum)
 {
-	u8 addr = RevPiDevice_getDev(i8uDevice_p)->i8uAddress;
+	int dev_idx;
+	u8 *snd_buf;
+	u8 addr;
 	int ret;
-	SIOGeneric sResponse_l;
-	INT8U i, len_l;
 
-	pr_info_aio("piAIOComm_Init %d of %d  addr %d\n", i8uDevice_p, i8uConfigured_s,
-		    RevPiDevice_getDev(i8uDevice_p)->i8uAddress);
+	addr = RevPiDevice_getDev(devnum)->i8uAddress;
 
-	for (i = 0; i < i8uConfigured_s; i++) {
-		if (aioConfig_s[i].uHeader.sHeaderTyp1.bitAddress == RevPiDevice_getDev(i8uDevice_p)->i8uAddress) {
-			pr_info_aio("piAIOComm_Init send configIn1\n");
-			ret = piIoComm_send((INT8U *) & aioIn1Config_s[i], sizeof(SAioInConfig));
-			if (ret == 0) {
-				len_l = 0;	// empty config telegram
+	pr_info_aio("piAIOComm_Init %d of %d  addr %d\n", devnum,
+		    num_aios, addr);
 
-				ret = piIoComm_recv((INT8U *) & sResponse_l, IOPROTOCOL_HEADER_LENGTH + len_l + 1);
-				if (ret > 0) {
-					if (piIoComm_response_valid(&sResponse_l, addr, len_l)) {
-						pr_info_aio("piAIOComm_Init done configIn1\n");
-						// success -> continue
-					} else {
-						return 1;	// wrong crc
-					}
-				} else {
-					return 2;	// no response
-				}
-			} else {
-				return 3;	// could not send
-			}
-
-			pr_info_aio("piAIOComm_Init send configIn2\n");
-			ret = piIoComm_send((INT8U *) & aioIn2Config_s[i], sizeof(SAioInConfig));
-			if (ret == 0) {
-				len_l = 0;	// empty config telegram
-
-				ret = piIoComm_recv((INT8U *) & sResponse_l, IOPROTOCOL_HEADER_LENGTH + len_l + 1);
-				if (ret > 0) {
-					if (piIoComm_response_valid(&sResponse_l, addr, len_l)) {
-						pr_info_aio("piAIOComm_Init done configIn2\n");
-						// success -> continue
-					} else {
-						return 1;	// wrong crc
-					}
-				} else {
-					return 2;	// no response
-				}
-			} else {
-				return 3;	// could not send
-			}
-
-
-			pr_info_aio("piAIOComm_Init send config\n");
-			ret = piIoComm_send((INT8U *) & aioConfig_s[i], sizeof(SAioConfig));
-			if (ret == 0) {
-				len_l = 0;	// empty config telegram
-
-				ret = piIoComm_recv((INT8U *) & sResponse_l, IOPROTOCOL_HEADER_LENGTH + len_l + 1);
-				if (ret > 0) {
-					if (piIoComm_response_valid(&sResponse_l, addr, len_l)) {
-						pr_info_aio("piAIOComm_Init done config\n");
-						return 0;	// success -> leave the function
-					} else {
-						return 1;	// wrong crc
-					}
-				} else {
-					return 2;	// no response
-				}
-			} else {
-				return 3;	// could not send
-			}
-		}
+	for (dev_idx = 0; dev_idx < num_aios; dev_idx++) {
+		if (aio_dev[dev_idx] == addr)
+			break;
 	}
 
-	return 4;		// unknown device
-}
+	if (dev_idx == num_aios)
+		return 4; // unknown device
 
+	snd_buf = (u8 *) &aioIn1Config_s[dev_idx];
+
+	pr_info_aio("piAIOComm_Init send configIn1\n");
+	ret = pibridge_req_io(addr, IOP_TYP1_CMD_DATA2, snd_buf,
+			      AIO_CONFIG_DATA2_LEN, NULL, 0);
+	if (ret)
+		return 3;
+
+	snd_buf = (u8 *) &aioIn2Config_s[dev_idx];
+
+	pr_info_aio("piAIOComm_Init send configIn2\n");
+	ret = pibridge_req_io(addr, IOP_TYP1_CMD_DATA3, snd_buf,
+			      AIO_CONFIG_DATA3_LEN, NULL, 0);
+	if (ret)
+		return 3;
+
+	snd_buf = (u8 *) &aioConfig_s[dev_idx];
+
+	pr_info_aio("piAIOComm_Init send config\n");
+	ret = pibridge_req_io(addr, IOP_TYP1_CMD_CFG, snd_buf,
+			      AIO_CONFIG_DATA1_LEN, NULL, 0);
+	if (ret)
+		return 3;
+
+	pr_info_aio("piAIOComm_Init done config\n");
+
+	return 0;
+}
 
 u32 piAIOComm_sendCyclicTelegram(u8 devnum)
 {
