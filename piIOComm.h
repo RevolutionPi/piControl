d848c0855621bce3716bc2cf49d9327de9a0f564
Nicolai Buchwitz <n.buchwitz@kunbus.com>
2023-03-08 19:41:45 +0100

    Connect 4: Use ttyAMA1 instead of ttyAMA0
    The RevPi Connect 4 uses ttyAMA1 for PiBridge RS485 instead of ttyAMA0, which
    is used on older RevPi devices. With the integrated bluetooth radio, which is
    internally connected to ttyAMA0 on the Raspberry CM4, we need a switch in order
    to support both interfaces (depending on device type).
    
    Signed-off-by: Nicolai Buchwitz <n.buchwitz@kunbus.com>

--- piIOComm.h
+++ piIOComm.h
@@ -42,7 +42,8 @@
 
 #define REV_PI_RECV_IO_HEADER_LEN	65530
 
-#define REV_PI_TTY_DEVICE	"/dev/ttyAMA0"
+#define REV_PI_TTY_DEVICE           "/dev/ttyAMA0"
+#define REV_PI_TTY_DEVICE_CONNECT_4 "/dev/ttyAMA1"
 
 enum IOSTATE {
     /* physically not connected */
