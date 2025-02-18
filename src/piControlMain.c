RevolutionPi/piControl

commit 5f2471a85ebc959f0e9a517ce01c7e0066321240

Ignore failure of piConfigParse()
With commit 2da3676616fe ("Evaluate return value of piConfigParse()") the
return value of piConfigParse() is not longer ignored. This leads to the
undesired behaviour that loading the piControl module fails in case of an
error in the configuration file.

Revert this change and thus restore the old behaviour in which piControl
loads even if the configuration file is incorrect.

Signed-off-by: Lino Sanfilippo <l.sanfilippo@kunbus.com>

src/piControlMain.c
@@ -267,13 +267,8 @@ static int __init piControlInit(void)
 	piDev_g.tLastOutput2 = ktime_set(0, 0);
 
 	/* start application */
-	res = piConfigParse(PICONFIG_FILE, &piDev_g.devs, &piDev_g.ent,
-			    &piDev_g.cl, &piDev_g.connl);
-	if (res) {
-		pr_err("Error parsing config!\n");
-		res = -ENXIO;
-		goto err_dev_destroy;
-	}
+	piConfigParse(PICONFIG_FILE, &piDev_g.devs, &piDev_g.ent, &piDev_g.cl,
+		      &piDev_g.connl);
 
 	if (piDev_g.pibridge_supported) {
 		res = revpi_core_init();
@@ -337,18 +332,18 @@ static int piControlReset(tpiControlInst * priv)
 	piCopylist *cl;
 	piConnectionList *connl;
 
-	/* start application */
-	if (piConfigParse(PICONFIG_FILE, &devs, &ent, &cl, &connl)) {
-		pr_err("\n\nError parsing config!\n\n");
-	} else {
-		kfree(piDev_g.devs);
-		kfree(piDev_g.ent);
-		kfree(piDev_g.cl);
 
-		piDev_g.devs = devs;
-		piDev_g.ent = ent;
-		piDev_g.cl = cl;
-	}
+	kfree(piDev_g.ent);
+	piDev_g.ent = NULL;
+
+	kfree(piDev_g.devs);
+	piDev_g.devs = NULL;
+
+	kfree(piDev_g.cl);
+	piDev_g.cl = NULL;
+
+	/* start application */
+	piConfigParse(PICONFIG_FILE, &devs, &ent, &cl, &connl);
 
 	if (piDev_g.machine_type == REVPI_COMPACT) {
 		revpi_compact_reset();

commit c12da3ef6ec6fa436a6e02dbeb8900c25e92b206

At piControl reset do not forget to store parsing results
In piControlReset() the results of piConfigParse() are only assigned to
local variables but then not assigned to the global variable tpiControlDev
from where they are accessed later.

Fix this by assigning the parsing results directly to tpiControlDev and
removing the useless local variables.

Fixes: 5f2471a85ebc ("Ignore failure of piConfigParse()")
Signed-off-by: Lino Sanfilippo <l.sanfilippo@kunbus.com>

src/piControlMain.c
@@ -327,11 +327,6 @@ static int piControlReset(tpiControlInst * priv)
 {
 	int status = -EFAULT;
 	int timeout = 10000;	// ms
-	piDevices *devs;
-	piEntries *ent;
-	piCopylist *cl;
-	piConnectionList *connl;
-
 
 	kfree(piDev_g.ent);
 	piDev_g.ent = NULL;
@@ -343,7 +338,8 @@ static int piControlReset(tpiControlInst * priv)
 	piDev_g.cl = NULL;
 
 	/* start application */
-	piConfigParse(PICONFIG_FILE, &devs, &ent, &cl, &connl);
+	piConfigParse(PICONFIG_FILE, &piDev_g.devs, &piDev_g.ent, &piDev_g.cl,
+		      &piDev_g.connl);
 
 	if (piDev_g.machine_type == REVPI_COMPACT) {
 		revpi_compact_reset();