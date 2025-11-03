RevolutionPi/piControl

commit 43b028beda4334005ce77ffff7407abdeb2ea820

Register platform device driver also for Flat and Compact
Beside for the RevPi Core/Connect register piControl also for Compact and
Flat as a platform device driver.
This allows to defer the piControl initialization (via the deferred probe()
mechanism) in case that one or more devices required by the Compact or
Flat implementation are not yet available at boot time.

Signed-off-by: Lino Sanfilippo <l.sanfilippo@kunbus.com>

src/piControlMain.c
@@ -14,6 +14,7 @@
 #include <linux/version.h>
 #include <linux/wait.h>
 #include <linux/firmware.h>
+#include <linux/platform_device.h>
 
 #include "IoProtocol.h"
 #include "ModGateRS485.h"
@@ -424,13 +425,11 @@ static void piControl_deinit_sysfs(void)
 	sysfs_remove_file(&piDev_g.dev->kobj, &dev_attr_cycle_duration.attr);
 }
 
-static int __init piControlInit(void)
+static int pibridge_probe(struct platform_device *pdev)
 {
 	int devindex = 0;
 	dev_t curdev;
 	int res;
-
-	wait_for_device_probe();
 
 	if (of_machine_is_compatible("kunbus,revpi-compact")) {
 		piDev_g.machine_type = REVPI_COMPACT;
@@ -607,12 +606,12 @@ static int __init piControlInit(void)
 		      &piDev_g.connl);
 
 	if (piDev_g.pibridge_supported) {
-		res = revpi_core_init();
+		res = revpi_core_probe(pdev);
 	} else { // standalone device
 		if (piDev_g.machine_type == REVPI_COMPACT)
-			res = revpi_compact_init();
+			res = revpi_compact_probe(pdev);
 		else if (piDev_g.machine_type == REVPI_FLAT)
-			res = revpi_flat_init();
+			res = revpi_flat_probe(pdev);
 	}
 
 	if (res)
@@ -637,12 +636,12 @@ err_revpi_fini:
 	if (piDev_g.pibridge_supported) {
 		if (isRunning())
 			PiBridgeMaster_Stop();
-		revpi_core_fini();
+		revpi_core_remove(pdev);
 	} else { // standalone devices
 		if (piDev_g.machine_type == REVPI_COMPACT)
-			revpi_compact_fini();
+			revpi_compact_remove(pdev);
 		else if (piDev_g.machine_type == REVPI_FLAT)
-			revpi_flat_fini();
+			revpi_flat_remove(pdev);
 	}
 err_free_config:
 	kfree(piDev_g.ent);
@@ -729,7 +728,11 @@ static int piControlReset(tpiControlInst * priv)
 	return status;
 }
 
-static void __exit piControlCleanup(void)
+#if KERNEL_VERSION(6, 11, 0) <= LINUX_VERSION_CODE
+static void pibridge_remove(struct platform_device *pdev)
+#else
+static int pibridge_remove(struct platform_device *pdev)
+#endif
 {
 	dev_t curdev;
 
@@ -740,12 +743,12 @@ static void __exit piControlCleanup(void)
 	if (piDev_g.pibridge_supported) {
 		if (isRunning())
 			PiBridgeMaster_Stop();
-		revpi_core_fini();
+		revpi_core_remove(pdev);
 	} else { // standalone devices
 		if (piDev_g.machine_type == REVPI_COMPACT)
-			revpi_compact_fini();
+			revpi_compact_remove(pdev);
 		else if (piDev_g.machine_type == REVPI_FLAT)
-			revpi_flat_fini();
+			revpi_flat_remove(pdev);
 	}
 
 	kfree(piDev_g.ent);
@@ -758,6 +761,10 @@ static void __exit piControlCleanup(void)
 
 	pr_debug("driver stopped with MAJOR-No. %d\n\n ", MAJOR(piControlMajor));
 	pr_debug("piControlCleanup done\n");
+
+#if KERNEL_VERSION(6, 11, 0) > LINUX_VERSION_CODE
+	return 0;
+#endif
 }
 
 // true: system is running
@@ -2048,6 +2055,38 @@ static long piControlIoctl(struct file *file, unsigned int prg_nr, unsigned long
 	}
 
 	return status;
+}
+
+static const struct of_device_id of_pibridge_match[] = {
+	{ .compatible = "kunbus,pibridge", },
+	{},
+};
+
+static struct platform_driver pibridge_driver = {
+	.probe		= pibridge_probe,
+	.remove		= pibridge_remove,
+	.driver		= {
+		.name	= "pibridge",
+		.of_match_table = of_pibridge_match,
+	},
+};
+
+static int __init piControlInit(void)
+{
+	int ret;
+
+	wait_for_device_probe();
+	ret = platform_driver_register(&pibridge_driver);
+	if (ret)
+		pr_err("failed to register pibridge driver: %i\n", ret);
+
+	return ret;
+}
+
+
+static void __exit piControlCleanup(void)
+{
+	platform_driver_unregister(&pibridge_driver);
 }
 
 module_init(piControlInit);

src/revpi_compact.c
@@ -14,6 +14,7 @@
 #include <linux/spi/max3191x.h>
 #include <linux/spi/spi.h>
 #include <linux/thermal.h>
+#include <linux/platform_device.h>
 
 #include "piControlMain.h"
 #include "process_image.h"
@@ -530,7 +531,7 @@ void revpi_compact_adjust_config(void)
 	kfree(state);
 }
 
-int revpi_compact_init(void)
+int revpi_compact_probe(struct platform_device *pdev)
 {
 	SRevPiCompact *machine;
 	struct device *dev;
@@ -713,7 +714,7 @@ err_remove_table:
 	return ret;
 }
 
-void revpi_compact_fini(void)
+void revpi_compact_remove(struct platform_device *pdev)
 {
 	SRevPiCompact *machine = (SRevPiCompact *)piDev_g.machine;
 

src/revpi_compact.h
@@ -6,6 +6,7 @@
 #define _REVPI_COMPACT_H
 
 #include <linux/types.h>
+#include <linux/platform_device.h>
 
 #include "common_define.h"
 #include "piControl.h"
@@ -74,8 +75,8 @@ struct revpi_compact_stats {
 };
 
 INT32U revpi_compact_config(uint8_t i8uAddress, uint16_t i16uNumEntries, SEntryInfo * pEnt);
-int revpi_compact_init(void);
 int revpi_compact_reset(void);
-void revpi_compact_fini(void);
+int revpi_compact_probe(struct platform_device *pdev);
+void revpi_compact_remove(struct platform_device *pdev);
 void revpi_compact_adjust_config(void);
 #endif /*_REVPI_COMPACT_H */

src/revpi_core.c
@@ -436,7 +436,7 @@ err_deinit_sniff_gpios:
 	return ret;
 }
 
-static int pibridge_probe(struct platform_device *pdev)
+int revpi_core_probe(struct platform_device *pdev)
 {
 	int ret;
 
@@ -488,46 +488,8 @@ err_deinit_gpios:
 	return ret;
 }
 
-#if KERNEL_VERSION(6, 11, 0) <= LINUX_VERSION_CODE
-static void pibridge_remove(struct platform_device *pdev)
-#else
-static int pibridge_remove(struct platform_device *pdev)
-#endif
+void revpi_core_remove(struct platform_device *pdev)
 {
 	kthread_stop(piCore_g.pIoThread);
 	deinit_gpios();
-
-#if KERNEL_VERSION(6, 11, 0) > LINUX_VERSION_CODE
-	return 0;
-#endif
-}
-
-static const struct of_device_id of_pibridge_match[] = {
-	{ .compatible = "kunbus,pibridge", },
-	{},
-};
-
-static struct platform_driver pibridge_driver = {
-	.probe		= pibridge_probe,
-	.remove 	= pibridge_remove,
-	.driver		= {
-		.name	= "pibridge",
-		.of_match_table = of_pibridge_match,
-	},
-};
-
-int revpi_core_init(void)
-{
-	int ret;
-
-	ret = platform_driver_register(&pibridge_driver);
-	if (ret)
-		pr_err("failed to register pibridge driver: %i\n", ret);
-
-	return ret;
-}
-
-void revpi_core_fini(void)
-{
-	platform_driver_unregister(&pibridge_driver);
 }

src/revpi_core.h
@@ -8,6 +8,7 @@
 #include <linux/netdevice.h>
 #include <linux/types.h>
 #include <linux/pibridge_comm.h>
+#include <linux/platform_device.h>
 
 #include "ModGateRS485.h"
 #include "piControlMain.h"
@@ -101,6 +102,6 @@ extern SRevPiCore piCore_g;
 
 u8 revpi_core_find_gate(struct net_device *netdev, u16 module_type);
 void revpi_core_gate_connected(SDevice *revpi_dev, bool connected);
-int revpi_core_init(void);
-void revpi_core_fini(void);
+int revpi_core_probe(struct platform_device *pdev);
+void revpi_core_remove(struct platform_device *pdev);
 #endif /* _REVPI_CORE_H */

src/revpi_flat.c
@@ -10,6 +10,7 @@
 #include <linux/thermal.h>
 #include <linux/types.h>
 #include <linux/version.h>
+#include <linux/platform_device.h>
 
 #include "piControlMain.h"
 #include "process_image.h"
@@ -297,7 +298,7 @@ int revpi_flat_reset(void)
 	return 0;
 }
 
-int revpi_flat_init(void)
+int revpi_flat_probe(struct platform_device *pdev)
 {
 	struct revpi_flat *flat;
 	unsigned int button_gpio;
@@ -408,7 +409,7 @@ err_put_ain:
 	return ret;
 }
 
-void revpi_flat_fini(void)
+void revpi_flat_remove(struct platform_device *pdev)
 {
 	struct revpi_flat *flat = (struct revpi_flat *) piDev_g.machine;
 

src/revpi_flat.h
@@ -5,8 +5,10 @@
 #ifndef _REVPI_FLAT_H
 #define _REVPI_FLAT_H
 
-int revpi_flat_init(void);
-void revpi_flat_fini(void);
+#include <linux/platform_device.h>
+
+int revpi_flat_probe(struct platform_device *pdev);
+void revpi_flat_remove(struct platform_device *pdev);
 int revpi_flat_reset(void);
 
 #endif /* _REVPI_FLAT_H */

commit 77f9636842f3119e574114dee86f7b7ccbc90fb9

avoid piControl_init_sysfs clean up before use
The "piControl_init_sysfs" function is marked with "__init" which
indicated it can be cleaned up after initialisation. The calling
function "pibridge_probe" is *not* an initialization function. This can
lead to "pibridge_probe" trying to call "piControl_init_sysfs", which is
already cleaned up and inaccessible.

Remove the "__init" marker from "piControl_init_sysfs" to avoid this.

Fixes: 43b028beda43 ("Register platform device driver also for Flat and Compact")
Signed-off-by: Thomas Böhler <t.boehler@kunbus.com>

src/piControlMain.c
@@ -364,7 +364,7 @@ static DEVICE_ATTR_RW(max_cycle_deviation);
 static DEVICE_ATTR_RW(cycles_exceeded);
 static DEVICE_ATTR_RW(cycles_missed);
 
-static int __init piControl_init_sysfs(void)
+static int piControl_init_sysfs(void)
 {
 	int ret;
 