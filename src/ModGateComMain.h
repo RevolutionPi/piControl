9da722f283b7ea96186f0239edf72d4257944a3e
Nicolai Buchwitz <n.buchwitz@kunbus.com>
2026-04-17 10:06:29 +0200

    piControl: remove unused declarations from ModGateComMain
    Remove all symbols from ModGateComMain.h that are not referenced by
    the kernel module:
    
     - MG_LL_* function declarations and sLLData/LLHandle types
     - sALData/ALHandle types and AL_Data_s extern variable
     - MODGATECOM_Packet struct
     - MODGATECOM_EPowerLedState enum
     - MODGATECOM_MAX_MODULES, MODGATE_feature_RS485DataExchange
     - MODGATE_LL_HEADER_LEN, MODGATE_TL_HEADER_LEN, MODGATE_AL_MAX_LEN
     - Entire #ifndef __KUNBUSPI_KERNEL__ block (26 function declarations,
       MODGATE_OwnID_g extern, accessor macros)
    
    These were remnants of the firmware-side mGate communication API with
    no implementation in the kernel module. Inline the intermediate size
    macros into MODGATE_LL_MAX_LEN which is the only one actually used.
    
    Signed-off-by: Nicolai Buchwitz <n.buchwitz@kunbus.com>

--- src/ModGateComMain.h
+++ src/ModGateComMain.h
@@ -63,16 +63,6 @@ typedef enum
 
 typedef enum
 {
-    MODGATECOM_enPLS_INIT                   = 0x01,
-    MODGATECOM_enPLS_LINK_MISSING           = 0x02,
-    MODGATECOM_enPLS_DATA_MISSING           = 0x03,
-    MODGATECOM_enPLS_RUN                    = 0x04,
-    MODGATECOM_enPLS_ERROR                  = 0x05,
-}  MODGATECOM_EPowerLedState;
-
-
-typedef enum
-{
     MODGATE_AL_CMD_ID_Req                   = 0x0001,
     MODGATE_AL_CMD_ID_Resp                  = 0x8001,
     MODGATE_AL_CMD_cyclicPD                 = 0x0002,
@@ -83,14 +73,8 @@ typedef enum
 
 // Feature descriptor bits
 #define MODGATE_feature_IODataExchange          0x0001 // supports data-exchange using ethernet (e.g. mGate)
-#define MODGATE_feature_RS485DataExchange       0x0002 // supports data exchange using RS485 (e.g. piDio)
 
-
-#define MODGATE_LL_HEADER_LEN               (sizeof(MODGATECOM_LinkLayer))  // 16
-#define MODGATE_TL_HEADER_LEN               (sizeof(MODGATECOM_LinkLayer) + sizeof(MODGATECOM_TransportLayer))  // 26
 #define MODGATE_MAX_PD_DATALEN              512
-#define MODGATE_AL_MAX_LEN                  (sizeof(MODGATECOM_CyclicPD) + MODGATE_MAX_PD_DATALEN)   // 543 size of the biggest AL packet
-#define MODGATE_LL_MAX_LEN                  ((MODGATE_TL_HEADER_LEN + MODGATE_AL_MAX_LEN + 3) & 0xfffffffc) // 544 bigger packet on the line, rounded up to the next multiple of 4
 
 //**********************************************************************************************
 typedef struct {
@@ -113,121 +97,6 @@ typedef struct {
     u8   i8uData[0];     // dummy declaration for up to MODGATE_MAX_PD_DATALEN bytes
 } __attribute__((__packed__)) MODGATECOM_CyclicPD;
 
-typedef struct {
-    MODGATECOM_LinkLayer      strLinkLayer;
-    MODGATECOM_TransportLayer strTransportLayer;
-    u8                     i8uData[MODGATE_AL_MAX_LEN];
-} __attribute__((__packed__)) MODGATECOM_Packet;
-
-//**********************************************************************************************
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-//**********************************************************************************************
-// Link Layer
-//**********************************************************************************************
-
-typedef struct _sLLData
-{
-    MODGATECOM_LinkLayer    Header;
-    u8                   state;
-    u32                  send_tick;       // tick counter of last sent packet
-    u8                   send_retry;      // retry counter
-    u32                  recv_tick;       // tick counter of last recv packet
-    bool                   timed_out;
-    MODGATECOM_Packet      *pLastData;
-} sLLData;
-
-typedef sLLData *LLHandle;
-
-u32 MG_LL_init (LLHandle llHdl);
-u32 MG_LL_send (LLHandle llHdl, MODGATECOM_Packet *pData_p);
-MODGATECOM_Packet *MG_LL_recv(LLHandle llHdl, u32 *pi32uStatus_p, u16 *pi16uLen_p);
-bool  MG_LL_pending (LLHandle llHdl);
-void   MG_LL_abort (LLHandle llHdl);
-bool  MG_LL_timed_out (LLHandle llHdl);
-
-//**********************************************************************************************
-// Application Layer
-//**********************************************************************************************
-typedef struct _sALData
-{
-    sLLData     llParas;
-
-    MODGATECOM_IDResp OtherID;              //!< ID-Data of other mGate
-
-    u8 *pi8uInData;
-    u16 i16uInDataLen;
-    u16 i16uInDataLenActive;
-
-    u8 *pi8uOutData;
-    u16 i16uOutDataLen;
-    u16 i16uOutDataLenActive;
-
-    u8    i8uState;                      //!< modular Gateway state machine state
-    u8    i8uOtherFieldbusState;         //!< Fieldbus State of other mGate
-    MODGATECOM_EPowerLedState enLedStateAct;
-    MODGATECOM_EPowerLedState enLedStateOld;
-} sALData;
-
-typedef sALData *ALHandle;
-
-extern sALData AL_Data_s[MODGATECOM_MAX_MODULES];
-
-//**********************************************************************************************
-#ifndef __KUNBUSPI_KERNEL__
-u32 MODGATECOM_init (u8 *pi8uInData_p,  u16 i16uInDataLen_p, u8 *pi8uOutData_p, u16 i16uOutDataLen_p, ETHERNET_INTERFACE *EthDrv);
-void   MODGATECOM_run (void);
-
-void   MODGATECOM_SetOwnFieldbusState(u8 i8uOwnFieldbusState_p);
-u8  MODGATECOM_GetOwnFieldbusState(void);
-u8  MODGATECOM_GetOtherFieldbusState(u8 i8uInst_p);               // in modular Gateways always 0 must be passed
-MODGATECOM_EPowerLedState MODGATECOM_GetLedState(void);
-
-u16 MODGATECOM_GetInputDataLengthActive(u8 i8uInstance_p);    // in modular Gateways, always 0 must be passed
-u16 MODGATECOM_GetOutputDataLengthActive(u8 i8uInstance_p);   // ditto
-
-//**********************************************************************************************
-// internal
-//**********************************************************************************************
-
-u32 MODGATECOM_send_ID_Req   (ALHandle);
-u32 MODGATECOM_send_ID_Resp  (ALHandle);
-u32 MODGATECOM_send_cyclicPD (ALHandle);
-
-bool MODGATECOM_recv_Id_Resp  (ALHandle, MODGATECOM_Packet *pPacket_p);
-bool MODGATECOM_recv_cyclicPD (ALHandle, MODGATECOM_Packet *pPacket_p);
-
-void   MODGATECOM_managePowerLedRun (void);
-MODGATE_AL_Status MODGATECOM_GetState(u8 i8uInst_p);
-
-//**********************************************************************************************
-
-extern MODGATECOM_IDResp MODGATE_OwnID_g;     //!< ID-Data of this mGate
-
-
-
-#define MODGATECOM_GetOtherFieldbusStatePtr(i)       (&AL_Data_s[i].i8uOtherFieldbusState)
-#define MODGATECOM_GetOwnIdDataPtr()                 (&MODGATE_OwnID_g)
-#define MODGATECOM_GetOtherIdDataPtr(i)              (&AL_Data_s[i].OtherID)
-
-
-// internal functions
-// for all functions: return value true is send/receive was successful
-bool  MODGATECOM_receive (void);
-bool  MODGATECOM_send (u16 cmd);
-bool  MODGATECOM_send_ACK (void);
-
-void   MODGATECOM_T1_Handler (void);
-void   MODGATECOM_T2_Handler (void);
-
-#endif // !__KUNBUSPI_KERNEL__
-
-#ifdef  __cplusplus
-}
-#endif
-
+#define MODGATE_LL_MAX_LEN                  ((sizeof(MODGATECOM_LinkLayer) + sizeof(MODGATECOM_TransportLayer) + sizeof(MODGATECOM_CyclicPD) + MODGATE_MAX_PD_DATALEN + 3) & 0xfffffffc)
 
 #endif //MODGATECOMMAIN_H_INC
