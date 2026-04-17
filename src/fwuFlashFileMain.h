856ca40b0052ebc9f5e86f0fd72c4e34322e03d7
Nicolai Buchwitz <n.buchwitz@kunbus.com>
2026-04-17 10:09:20 +0200

    piControl: remove unused declarations from fwuFlashFileMain
    Remove all symbols not referenced by the kernel module:
    
     - FWU_FS_main(), FWU_BuR_main(), FWU_check_file(), FWU_error()
       function declarations (no implementation in this tree)
     - FWU_CODE_SECTION macro (expands to nothing, never used)
     - FWU_FILENAME_PATTERN* and FWU_FILENAME_MODE* macros
     - __STM32GENERIC__ and __cplusplus conditionals
    
    Only the TFileHead/TFileHeadData/TFPGAHeadData structs remain, used
    by piFirmwareUpdate.c to parse .fwu firmware file headers.
    
    Signed-off-by: Nicolai Buchwitz <n.buchwitz@kunbus.com>

--- src/fwuFlashFileMain.h
+++ src/fwuFlashFileMain.h
@@ -2,32 +2,10 @@
  * SPDX-FileCopyrightText: 2017-2023 KUNBUS GmbH
  */
 
-//+=============================================================================================
-//|
-//!		\file fwuMain.h
-//!
-//!		Firmware update main routines.
-//|
-//+---------------------------------------------------------------------------------------------
-//|
-//|		Files required:	(none)
-//|
-//+=============================================================================================
 #ifndef FWUFLASHFILEMAIN_H_INC
 #define FWUFLASHFILEMAIN_H_INC
 
-#if defined(__STM32GENERIC__)
-    #define FWU_CODE_SECTION    __attribute__ ((section(".firmwareUpdate")))
-#else // Common Define for Visual Studio / WIN32
-    #define FWU_CODE_SECTION
-#endif
-
 #include "common_define.h"
-
-#ifdef __cplusplus
-extern "C" { 
-#endif 
-
 
 typedef struct StrFileHeadData {
     u16 usType;
@@ -51,29 +29,4 @@ typedef struct StrFileHead {
     TFPGAHeadData fpga;
 } __attribute__((__packed__)) TFileHead; // sizeof = 36
 
-
-#define FWU_FILENAME_PATTERN		"/*.kfu"
-#define FWU_FILENAME_PATTERN_0		'/'
-#define FWU_FILENAME_PATTERN_1		'*'
-#define FWU_FILENAME_PATTERN_2		'.'
-#define FWU_FILENAME_PATTERN_3		'k'
-#define FWU_FILENAME_PATTERN_4		'f'
-#define FWU_FILENAME_PATTERN_5		'u'
-#define FWU_FILENAME_PATTERN_6		'\0'
-#define FWU_FILENAME_MODE_0			'r'
-#define FWU_FILENAME_MODE_1			'b'
-#define FWU_FILENAME_MODE_2			'\0'
-
-extern void FWU_FS_main (void *file, TFileHead *header, u8 write_fpga) FWU_CODE_SECTION;
-extern void FWU_BuR_main(u32 i32uAppPrgStartAdd, u32 i32uAppPrgSize) FWU_CODE_SECTION;
-extern bool FWU_check_file(void *file, TFileHead *header) FWU_CODE_SECTION;
-extern void FWU_error (u16 i16uErrorCode) FWU_CODE_SECTION;
-
-#ifdef  __cplusplus 
-} 
-#endif 
-
 #endif // FWUFLASHFILEMAIN_H_INC
-
-
-
